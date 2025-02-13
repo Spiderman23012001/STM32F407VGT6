3855beef73e752b4619db59ae7181743ab7e1688
Andrew <trantin23012001@gmail.com>
2025-02-13 08:55:03 +0700

    SPI- Define base addr

---
+++ SPI_driver/Debug/Drivers/Src/stm32f407xx_gpio.d
@@ -0,0 +1,5 @@
+Drivers/Src/stm32f407xx_gpio.o: ../Drivers/Src/stm32f407xx_gpio.c \
+ C:/Users/Rambo/Desktop/stm32/SPI_driver/Drivers/Inc/stm32f407xx_gpio.h \
+ C:/Users/Rambo/Desktop/stm32/SPI_driver/Drivers/Inc/stm32f407xx.h
+C:/Users/Rambo/Desktop/stm32/SPI_driver/Drivers/Inc/stm32f407xx_gpio.h:
+C:/Users/Rambo/Desktop/stm32/SPI_driver/Drivers/Inc/stm32f407xx.h:
---
+++ SPI_driver/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/Rambo/Desktop/stm32/SPI_driver/Drivers/Inc/stm32f407xx.h \
+ C:/Users/Rambo/Desktop/stm32/SPI_driver/Drivers/Inc/stm32f407xx_gpio.h \
+ C:/Users/Rambo/Desktop/stm32/SPI_driver/Drivers/Inc/stm32f407xx.h
+C:/Users/Rambo/Desktop/stm32/SPI_driver/Drivers/Inc/stm32f407xx.h:
+C:/Users/Rambo/Desktop/stm32/SPI_driver/Drivers/Inc/stm32f407xx_gpio.h:
+C:/Users/Rambo/Desktop/stm32/SPI_driver/Drivers/Inc/stm32f407xx.h:
